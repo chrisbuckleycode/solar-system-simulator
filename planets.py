97ab4f4cae9c8f3bdbb73310c8bf0197139c7079
Chris Buckley <62516125+chrisbuckleycode@users.noreply.github.com>
2023-01-01 00:45:18 +0100

    feat: Base - single planet stable animation

--- planets.py
+++ planets.py
@@ -39,22 +39,21 @@ y = 100                     # y position, center of circle
 radius = 20
 width = 0                   # zero radius = fill circle
 
-#Avo = 6.02e23
-Avo = 100000000000000
-softening = 0.1
+Avo = 400
+softening = 0.0000001
 
 # The Sun
-sun_radius = 50
-sun_x, sun_y = 400, 400
+sun_radius = 5
+sun_x, sun_y = 400, 400 # temporary assignment for testing
 #sun_x, sun_y = width // 2, height // 2  # center sun on screen, uses floor division to round down
 sun_mass = 1.989e30
 
 # Venus
-venus_radius = 20
+venus_radius = 2
 venus_mass = 4.87e24
-venus_x, venus_y = sun_x, sun_y - 100   # x, y coordinates
-venus_vx, venus_vy = 400, 0           # velocities
-venus_ax, venus_ay = 0, 0               # accelerations
+venus_x, venus_y = sun_x, sun_y - 35            # x, y coordinates
+venus_vx, venus_vy = 8500000000000, 0           # velocities
+venus_ax, venus_ay = 0, 1000000                 # accelerations
 
 # Earth
 earth_radius = 25
@@ -72,16 +71,8 @@ mars_ax, mars_ay = 0, 0
 
 # Time variables
 time_elapsed = 0
-time_step = 0.000000000000000000000008
+time_step = 0.0000000000005
 
-
-
-
-# INITIAL CONDITIONS
-
-#background = RED
-#screen.fill(background)
-#pygame.display.update()
 
 running = True
 # The event loop
@@ -104,70 +95,44 @@ while running:
     x += v / fps    # New x-position iterated (pixels)  
     pygame.draw.circle(screen, RED, (x, y), radius, width)  # Draw object in memory
 
-
     # Draw sun
     pygame.draw.circle(screen, WHITE, (sun_x, sun_y), sun_radius)
 
     # Draw Venus
     pygame.draw.circle(screen, WHITE, (venus_x, venus_y), venus_radius)
-    #pygame.draw.ellipse(screen, WHITE, (sun_x - 100, sun_y - 150, 200, 300), 1)
 
     # Draw Earth
-    pygame.draw.circle(screen, WHITE, (earth_x, earth_y), earth_radius)
-    #pygame.draw.ellipse(screen, WHITE, (sun_x - 200, sun_y - 250, 400, 500), 1)
+    #pygame.draw.circle(screen, WHITE, (earth_x, earth_y), earth_radius)
 
     # Draw Mars
-    pygame.draw.circle(screen, WHITE, (mars_x, mars_y), mars_radius)
-    #pygame.draw.ellipse(screen, WHITE, (sun_x - 300, sun_y - 350, 600, 700), 1)
+    #pygame.draw.circle(screen, WHITE, (mars_x, mars_y), mars_radius)
 
     pygame.display.flip()   # Redraw i.e. Update the full display Surface to the screen
 
-    # Update planet positions using Kepler's laws of planetary motion
-
-
+    # Update planet positions using Newton's universal law of gravitation.
     # F = m1a = Gm1m2/r^2
     # a = Gm2/r^2
     # Planet acceleration = G * sun_mass / distancebetween squared
     # Velocity update = acceleration * dt
 
-    venus_ax += -Avo * sun_mass / (((venus_x - sun_x) + softening)**2)
-    venus_ay += -Avo * sun_mass / (((venus_y - sun_y) + softening)**2)
-    venus_vx += venus_ax * time_step
-    venus_vy += venus_ay * time_step
+    venus_dx = venus_x - sun_x
+    venus_dy = venus_y - sun_y
+    venus_dist = math.hypot(venus_dx, venus_dy) # distance between two bodies
+    venus_theta = math.atan2(venus_dy, venus_dx) # angle between two bodies i.e. inverse tangent
+    venus_a = -Avo * venus_mass / ((venus_dist + softening)**2) # centripetal acceleration
+    venus_ax = venus_a * math.cos(venus_theta) # x-component of above
+    venus_ay = venus_a * math.sin(venus_theta) # y-component of above
+    venus_vx += venus_ax * time_step    # x-component velocity
+    venus_vy += venus_ay * time_step    # y-component velocity
     venus_x += venus_vx * time_step     # position = velocity * time step
     venus_y += venus_vy * time_step
 
-    earth_ax += Avo * sun_mass / (((earth_x - sun_x) + softening)**2)
-    earth_ay += Avo * sun_mass / (((earth_y - sun_y) + softening)**2)
-    earth_vx += earth_ax * time_step
-    earth_vy += earth_ay * time_step
-    earth_x += earth_vx * time_step
-    earth_y += earth_vy * time_step
-    
-    mars_ax += Avo * sun_mass / (((mars_x - sun_x) + softening)**2)
-    mars_ay += Avo * sun_mass / (((mars_y - sun_y) + softening)**2)
-    mars_vx += mars_ax * time_step
-    mars_vy += mars_ay * time_step
-    mars_x += mars_vx * time_step
-    mars_y += mars_vy * time_step
 
     # Update time elapsed
     time_elapsed += time_step
-
-
-
-
-
-
-
-
-
-
-
 
 
     clock.tick(fps) # delay by 1/fps seconds
      
 
 pygame.quit()
-
